3ac57fbc7fec353987d0edfda9402b6bf9be9772
Bhavin Ondhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2023-03-21 12:34:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing", {
-    strings: ["", "Software Developer", "Web Developer", "Android Developer", "ML Enthusiast", "DSA Lover"],
+    strings: ["", "Software Developer", "Web Developer", "Backend Developer", "ML Enthusiast", "DSA Lover"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
